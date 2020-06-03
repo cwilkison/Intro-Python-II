c648bf9cec94c8f8df0b14eb4fa4320c8c9990c3
Cole Wilkison <wilkison.cole@gmail.com>
2020-06-03 18:09:17 -0400

    almost at mvp

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -39,8 +40,38 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+player = Player('Cole', room['outside'])
+
 # Write a loop that:
 #
+
+while True:
+    direction = input("Where you going n, e, s, w? press 'q' to quit: ")
+    if direction == "n":
+        if player.current_room == None:
+            continue 
+        player.current_room = player.current_room.n_to
+    elif direction == "e":
+        if player.current_room == None:
+            continue
+        player.current_room = player.current_room.e_to
+    elif direction == "s":
+        if player.current_room == None:
+            continue
+        player.current_room = player.current_room.s_to
+    elif direction == "w":
+        if player.current_room == None:
+            continue
+        player.current_room = player.current_room.w_to
+    elif direction == "q":
+        print("You quit!")
+        break
+    else:
+        print("Please choose a direction")
+
+    print(player.current_room)
+
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
